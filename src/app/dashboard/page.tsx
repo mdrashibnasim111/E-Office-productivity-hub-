c4f62236cedba3ec01e58b991bb8cef26258f9a8
mdrashibnasim <mdrashibnasim111@gmail.com>
2025-10-01 20:26:41 +0000

    auto compress dashboard as per screen size

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,16 +14,16 @@ export default function DashboardPage() {
       <h1 className="text-2xl md:text-3xl font-bold tracking-tight font-headline">
         Welcome to your Dashboard
       </h1>
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-        <div className="lg:col-span-1">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="lg:col-span-1 md:col-span-2">
             <StreaksCard />
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:col-span-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:col-span-3">
             <StatsCards />
         </div>
       </div>
-      <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="xl:col-span-2">
+      <div className="grid gap-4 md:gap-8 lg:grid-cols-3">
+        <div className="lg:col-span-2">
             <ProductivityChart />
         </div>
         <div className="space-y-4">
