1a752738b94cc47c3a223e8fc5a5dfc3237f1093
mdrashibnasim <mdrashibnasim111@gmail.com>
2025-10-01 21:15:18 +0000

    CHANGE ALL ICON COLOR TO #11E0E0 (_for element <HardHat>_)

--- src/app/select-role/page.tsx
+++ src/app/select-role/page.tsx
@@ -108,7 +108,7 @@ export default function SelectRolePage() {
               >
                 <CardHeader className="flex-1 p-6">
                   <div className="flex justify-between items-start">
-                    <category.icon className="h-10 w-10 mb-4 text-[#11DBDC]" />
+                    <category.icon className="h-10 w-10 mb-4 text-[#11E0E0]" />
                     <ChevronRight className="h-6 w-6 text-muted-foreground transition-transform group-hover:translate-x-1" />
                   </div>
                   <CardTitle className="font-headline text-xl">{category.name}</CardTitle>
